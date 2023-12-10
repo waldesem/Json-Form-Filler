3d0faa74346e404e659e502b355589bfca8200ac
Vladimir Semenenko <wsemenenko@gmail.com>
2023-12-10 09:50:04 +0300

    Add TS

--- Json-Form-Filler/popup.js
+++ Json-Form-Filler/popup.js
@@ -1,85 +1,96 @@
 "use strict";
-
-document.getElementById('uploadForm').addEventListener('change', handleUpload);
-
-async function handleUpload() {
-  const checkBox = document.getElementById('checkbox').checked;
-  const files = Array.from(document.getElementById('upload').files);
-
-  if (!files) {
-    return;
-  };
-
-  const lenFiles = files.length;
-  if (!checkBox && lenFiles > 1) {
-    const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-    
-    await chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-
-      func: () => {
-        alert('Отправка нескольких файлов возможна только в автоматическом режиме');
-          return;
-        }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-    return;
-  };
-
-  files.forEach(async (file) => {
-    const reader = new FileReader();
-    reader.readAsText(file);
-    
-    const jsonData = await new Promise((resolve) => {
-      reader.onload = () => {
-        resolve(JSON.parse(reader.result));
-      };
-    });
-    
-    const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-    
-    await chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-
-      func: (jsonData, checkBox) => {
-        const dataForm = document.forms['checkform'];
-        if (!dataForm) {
-          return;
+};
+const uploadForm = document.getElementById('uploadForm');
+uploadForm.addEventListener('change', handleUpload);
+function handleUpload() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const checkBox = document.getElementById('checkbox').checked;
+        const uploadInput = document.getElementById('upload');
+        const filesElement = uploadInput.files;
+        let files = [];
+        if (filesElement) {
+            files = Array.from(filesElement);
         }
-        const fieldMap = {
-          'lastName': 'last_name',
-          'firstName': 'first_name',
-          'midName': 'patronymic',
-          'birthday': 'date',
-          'passportSerial': 'passport_series',
-          'passportNumber': 'passport_number',
-          'passportIssueDate': 'issueDate',
-          'inn': 'inn',
-          'snils': 'snils',
-          'contactPhone': 'mobile_phone',
-          'email': 'email'
-        };
-        Object.keys(jsonData).forEach(element => {
-          if (fieldMap[element]) {
-            if (['date','issueDate'].includes(fieldMap[element])) {
-              if (new Date(jsonData[element]) > new Date()) {
-                alert('Некорректная дата');
-                return;
-              };
-              dataForm[fieldMap[element]].value = new Date(jsonData[element]).toLocaleDateString('ru-RU');
-            } else {
-              dataForm[fieldMap[element]].value = jsonData[element];
-            }
-          }
-        });
-        // Submit the form
-        if (checkBox) {
-          dataForm.submit();
-          console.log(`Отправка формы для ${jsonData['lastName']} выполнена`);
-        } else {
-          console.log('Форма заполнена');
+        else {
+            return;
         }
-      },
-      args: [jsonData, checkBox],
+        ;
+        if (!checkBox && files.length > 1) {
+            const [tab] = yield chrome.tabs.query({ active: true, currentWindow: true });
+            yield chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                func: () => {
+                    alert('Отправка нескольких файлов возможна только в автоматическом режиме');
+                    return;
+                }
+            });
+            return;
+        }
+        ;
+        files.forEach((file) => __awaiter(this, void 0, void 0, function* () {
+            const reader = new FileReader();
+            reader.readAsText(file);
+            const jsonData = yield new Promise((resolve) => {
+                reader.onload = () => {
+                    resolve(JSON.parse(reader.result));
+                };
+            });
+            const [tab] = yield chrome.tabs.query({ active: true, currentWindow: true });
+            yield chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                func: (jsonData, checkBox) => {
+                    const dataForm = document.forms['checkform'];
+                    if (!dataForm) {
+                        return;
+                    }
+                    const fieldMap = {
+                        'lastName': 'last_name',
+                        'firstName': 'first_name',
+                        'midName': 'patronymic',
+                        'birthday': 'date',
+                        'passportSerial': 'passport_series',
+                        'passportNumber': 'passport_number',
+                        'passportIssueDate': 'issueDate',
+                        'inn': 'inn',
+                        'snils': 'snils',
+                        'contactPhone': 'mobile_phone',
+                        'email': 'email'
+                    };
+                    Object.keys(jsonData).forEach(element => {
+                        if (fieldMap[element]) {
+                            if (['date', 'issueDate'].includes(fieldMap[element])) {
+                                if (new Date(jsonData[element]) > new Date()) {
+                                    alert(`Некорректная дата для ${jsonData['lastName']}`);
+                                    return;
+                                }
+                                ;
+                                dataForm[fieldMap[element]].value = new Date(jsonData[element]).toLocaleDateString('ru-RU');
+                            }
+                            else {
+                                dataForm[fieldMap[element]].value = jsonData[element];
+                            }
+                        }
+                    });
+                    // Submit the form
+                    if (checkBox) {
+                        dataForm.submit();
+                        console.log(`Отправка формы для ${jsonData['lastName']} выполнена`);
+                    }
+                    else {
+                        console.log('Форма заполнена');
+                    }
+                },
+                args: [jsonData, checkBox],
+            });
+        }));
     });
-  })
 }
+;
---
+++ Json-Form-Filler/popup_legacy.js
@@ -0,0 +1,84 @@
+"use strict";
+
+document.getElementById('uploadForm').addEventListener('change', handleUpload);
+
+async function handleUpload() {
+  const checkBox = document.getElementById('checkbox').checked;
+  const files = Array.from(document.getElementById('upload').files);
+
+  if (!files) {
+    return;
+  };
+
+  if (!checkBox && files.length > 1) {
+    const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+    
+    await chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+
+      func: () => {
+        alert('Отправка нескольких файлов возможна только в автоматическом режиме');
+          return;
+        }
+    });
+    return;
+  };
+
+  files.forEach(async (file) => {
+    const reader = new FileReader();
+    reader.readAsText(file);
+    
+    const jsonData = await new Promise((resolve) => {
+      reader.onload = () => {
+        resolve(JSON.parse(reader.result));
+      };
+    });
+    
+    const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+    
+    await chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+
+      func: (jsonData, checkBox) => {
+        const dataForm = document.forms['checkform'];
+        if (!dataForm) {
+          return;
+        }
+        const fieldMap = {
+          'lastName': 'last_name',
+          'firstName': 'first_name',
+          'midName': 'patronymic',
+          'birthday': 'date',
+          'passportSerial': 'passport_series',
+          'passportNumber': 'passport_number',
+          'passportIssueDate': 'issueDate',
+          'inn': 'inn',
+          'snils': 'snils',
+          'contactPhone': 'mobile_phone',
+          'email': 'email'
+        };
+        Object.keys(jsonData).forEach(element => {
+          if (fieldMap[element]) {
+            if (['date','issueDate'].includes(fieldMap[element])) {
+              if (new Date(jsonData[element]) > new Date()) {
+                alert(`Некорректная дата для ${jsonData['lastName']}`);
+                return;
+              };
+              dataForm[fieldMap[element]].value = new Date(jsonData[element]).toLocaleDateString('ru-RU');
+            } else {
+              dataForm[fieldMap[element]].value = jsonData[element];
+            }
+          }
+        });
+        // Submit the form
+        if (checkBox) {
+          dataForm.submit();
+          console.log(`Отправка формы для ${jsonData['lastName']} выполнена`);
+        } else {
+          console.log('Форма заполнена');
+        }
+      },
+      args: [jsonData, checkBox],
+    });
+  });
+};
